9334d3fd0d8c92e32fd1e6a23d2396d45ecfc403
David Perez <perez.david3001@gmail.com>
2024-01-25 15:09:01 -0600

    Build a responsive layout

--- portfolio-site/src/App.tsx
+++ portfolio-site/src/App.tsx
@@ -1,7 +1,21 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">hello</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "main main"`, // lg is wider than 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="main" bg="yellow">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
