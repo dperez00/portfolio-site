6c21865d96260ac7609d675fbb0b58eee8c89a25
David Perez <perez.david3001@gmail.com>
2024-02-08 00:28:49 -0600

    Update HeroSection and NavBar components

--- portfolio-site/src/components/HeroSection.tsx
+++ portfolio-site/src/components/HeroSection.tsx
@@ -1,11 +1,21 @@
-import { Avatar, Text, HStack } from "@chakra-ui/react";
+import { Avatar, Text, HStack, Heading, VStack, Box } from "@chakra-ui/react";
 import AvatarLogo from "../assets/Avatar Transparent.png";
 
 const HeroSection = () => {
   return (
     <HStack justifyContent="center">
-      <Text>Hello, I'm David Perez</Text>
-      <Avatar size="2xl" bg="green.500" name="David Perez" src={AvatarLogo} />
+      <VStack>
+        <Heading as="h1" size="4xl" color="green.300" padding="35px" mb="-50px">
+          David Perez
+        </Heading>
+        <Text fontSize="5xl">
+          I'm a{" "}
+          <Box as="span" color="green.300">
+            Developer
+          </Box>
+        </Text>
+      </VStack>
+      <Avatar size="3xl" bg="green.300" name="David Perez" src={AvatarLogo} />
     </HStack>
   );
 };
