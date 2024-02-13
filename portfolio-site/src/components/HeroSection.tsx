d92fd0f0b4736b8239be79429cc7a61a603c861b
David Perez <perez.david3001@gmail.com>
2024-02-12 23:19:19 -0600

    Add dynamic word display in HeroSection component

--- portfolio-site/src/components/HeroSection.tsx
+++ portfolio-site/src/components/HeroSection.tsx
@@ -1,11 +1,29 @@
 import { Avatar, Text, HStack, Heading, VStack, Box } from "@chakra-ui/react";
 import AvatarLogo from "../assets/Avatar Transparent.png";
+import { useEffect, useState } from "react";
 
 const HeroSection = () => {
+  const [word, setWord] = useState("Developer");
+  const words = ["React Developer", "Shopify Developer"];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setWord((prevWord) => {
+        const currentIndex = words.indexOf(prevWord);
+        if (currentIndex === words.length - 1) {
+          return words[0];
+        } else {
+          return words[currentIndex + 1];
+        }
+      });
+    }, 2000); // change word every 2 seconds
+    return () => clearInterval(intervalId); // Clean up on component unmount
+  }, []);
+
   return (
     <HStack justifyContent="center">
       <VStack>
-        <Heading as="h1" size="4xl" padding="35px" mb="-50px">
+        <Heading as="h1" size="4xl" padding="35px" mb="-50px" mr="20px">
           David Perez
         </Heading>
         <Text fontSize="5xl">
@@ -15,7 +33,7 @@ const HeroSection = () => {
             bgGradient="linear(to-l, #7928CA, green.300)"
             bgClip="text"
           >
-            Developer
+            {word}
           </Box>
         </Text>
       </VStack>
