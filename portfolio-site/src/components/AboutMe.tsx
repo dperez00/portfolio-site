80b84eeba9e95751b06b88b7792d1d5c9bb5438f
David Perez <perez.david3001@gmail.com>
2024-03-05 21:12:40 -0600

    Add margin and background color to AboutMe component

--- portfolio-site/src/components/AboutMe.tsx
+++ portfolio-site/src/components/AboutMe.tsx
@@ -2,7 +2,7 @@ import { HStack, Heading } from "@chakra-ui/react";
 
 const AboutMe = () => {
   return (
-    <HStack justifyContent="center">
+    <HStack justifyContent="center" mt="50px" bgColor="#67d391">
       <Heading as="h2" size="2xl" mb="4">
         About Me
       </Heading>
