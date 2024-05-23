e4488e9875d854535c33c1f0c7dc0b8d4e5808a5
David Perez <perez.david3001@gmail.com>
2024-05-22 19:20:47 -0500

    Add images to project cards in Projects component

--- portfolio-site/src/components/Projects.tsx
+++ portfolio-site/src/components/Projects.tsx
@@ -2,9 +2,9 @@ import {
   Button,
   Card,
   CardBody,
-  CardHeader,
   HStack,
   Heading,
+  Image,
   Text,
 } from "@chakra-ui/react";
 
@@ -12,24 +12,40 @@ import {
 interface Project {
   title: string;
   description: string;
+  image: string;
 }
+
+const BRC = new URL("../assets/bottomrackclothing.png", import.meta.url);
+const CosmosForge = new URL("../assets/cosmosforge.png", import.meta.url);
+const CosmosForgeAffiliate = new URL(
+  "../assets/cosmosforgeaffiliate.png",
+  import.meta.url
+);
+const BeaningfulCoffee = new URL(
+  "../assets/beaningfulcoffee.png",
+  import.meta.url
+);
 
 const projects: Project[] = [
   {
     title: "Bottom Rack Clothing",
     description: "Web Design & Development",
+    image: BRC.toString(),
   },
   {
     title: "Cosmos Forge",
     description: "Web Design & Development",
+    image: CosmosForge.toString(),
   },
   {
     title: "Cosmos Forge Affiliate",
     description: "Theme Customization",
+    image: CosmosForgeAffiliate.toString(),
   },
   {
     title: "Beaningful Coffee",
     description: "Web Design & Development",
+    image: BeaningfulCoffee.toString(),
   },
 ];
 
@@ -47,8 +63,16 @@ const Projects = () => {
       <HStack justifyContent="center">
         {projects.map((project, index) => (
           <Card key={index}>
-            <CardHeader>{project.title}</CardHeader>
             <CardBody>
+              <Image
+                src={project.image}
+                alt={project.title}
+                boxSize="300px"
+                objectFit="fill"
+              />
+              <Heading as="h3" fontSize="24px">
+                {project.title}
+              </Heading>
               <Text>{project.description}</Text>
             </CardBody>
             <Button backgroundColor="#67d391">View Project</Button>
