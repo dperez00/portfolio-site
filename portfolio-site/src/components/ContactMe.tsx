5430ffee3b428a8f54599331b03696bd88d0ea0f
David Perez <perez.david3001@gmail.com>
2024-05-29 21:54:21 -0500

    Add email and LinkedIn icons to ContactMe component

--- portfolio-site/src/components/ContactMe.tsx
+++ portfolio-site/src/components/ContactMe.tsx
@@ -1,4 +1,6 @@
 import { Box, HStack, Heading, Text } from "@chakra-ui/react";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa";
 
 const ContactMe = () => {
   return (
@@ -12,10 +14,16 @@ const ContactMe = () => {
         </Heading>
       </HStack>
       <HStack justifyContent="center">
-        <Box border="1px" borderColor="green" borderRadius="32px">
-          <HStack gap="8px" margin="16px">
-            <Text fontSize="large">Email</Text>
-            <Text fontSize="large">LinkedIn</Text>
+        <Box border="2px" borderColor="#67d391" borderRadius="32px">
+          <HStack gap="16px" margin="16px">
+            <HStack>
+              <MdOutlineMailOutline />
+              <Text fontSize="large">perez.david3001@gmail.com</Text>
+            </HStack>
+            <HStack>
+              <FaLinkedin />
+              <Text fontSize="large">LinkedIn</Text>
+            </HStack>
           </HStack>
         </Box>
       </HStack>
