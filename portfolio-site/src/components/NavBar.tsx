1d5d5cc239ce49585fa62769d5b5e32848e7b514
David Perez <perez.david3001@gmail.com>
2024-06-24 21:31:25 -0500

    Refactor NavBar component to include navigation links

--- portfolio-site/src/components/NavBar.tsx
+++ portfolio-site/src/components/NavBar.tsx
@@ -1,12 +1,29 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/Logo Transparent.png";
 import ColorModeSwitch from "./ColorModeSwitch";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="70px" />
-      <ColorModeSwitch />
+      <HStack spacing="20px">
+        <Link to="/" style={{ fontSize: "24px" }}>
+          Home
+        </Link>
+        <Link to="/about" style={{ fontSize: "24px" }}>
+          About
+        </Link>
+        <Link to="/projects" style={{ fontSize: "24px" }}>
+          Projects
+        </Link>
+        <Link to="/contact" style={{ fontSize: "24px" }}>
+          Contact
+        </Link>
+      </HStack>
+      <HStack>
+        <ColorModeSwitch />
+      </HStack>
     </HStack>
   );
 };
