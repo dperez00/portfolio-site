3938144892b94b93ada72d77853c7c14f1a2eb58
David Perez <perez.david3001@gmail.com>
2024-06-25 21:54:46 -0500

    feat: Update react-scroll dependency to latest version for smooth scrolling

--- portfolio-site/src/components/NavBar.tsx
+++ portfolio-site/src/components/NavBar.tsx
@@ -1,23 +1,51 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/Logo Transparent.png";
 import ColorModeSwitch from "./ColorModeSwitch";
-import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="70px" />
       <HStack spacing="20px">
-        <Link to="/" style={{ fontSize: "24px" }}>
+        <Link
+          to="home"
+          smooth={true}
+          duration={500}
+          style={{ fontSize: "24px" }}
+        >
           Home
         </Link>
-        <Link to="/about" style={{ fontSize: "24px" }}>
+        <Link
+          to="about"
+          smooth={true}
+          duration={500}
+          style={{ fontSize: "24px" }}
+        >
           About
         </Link>
-        <Link to="/projects" style={{ fontSize: "24px" }}>
+        <Link
+          to="experience"
+          smooth={true}
+          duration={500}
+          style={{ fontSize: "24px" }}
+        >
+          Experience
+        </Link>
+        <Link
+          to="projects"
+          smooth={true}
+          duration={500}
+          style={{ fontSize: "24px" }}
+        >
           Projects
         </Link>
-        <Link to="/contact" style={{ fontSize: "24px" }}>
+        <Link
+          to="contact"
+          smooth={true}
+          duration={500}
+          style={{ fontSize: "24px" }}
+        >
           Contact
         </Link>
       </HStack>
