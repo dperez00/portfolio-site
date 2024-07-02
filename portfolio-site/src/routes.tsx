3ee23b899eacd7f7ee72f37c148d95f80b04e828
David Perez <perez.david3001@gmail.com>
2024-07-01 21:50:58 -0500

    feat: Add BottomRackClothing page and update routing

--- portfolio-site/src/routes.tsx
+++ portfolio-site/src/routes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
+import BottomRackClothing from "./pages/BottomRackClothing";
 
 /* We switched from using "Router" in this component to using "Switch" and "Route".
 We did this because we can't use "Router" in this component and in our Main.tsx file.
@@ -11,6 +12,8 @@ const AppRouter = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/BottomRackClothing" element={<BottomRackClothing />} />
+        {/* add more routes for other projects if neccessary */}
       </Routes>
     </>
   );
