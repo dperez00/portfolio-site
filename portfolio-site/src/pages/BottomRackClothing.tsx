fd870ae3ca212ba3afa78af0329288c9f2bc7fab
David Perez <perez.david3001@gmail.com>
2024-07-07 14:20:41 -0500

    Update BottomRackClothing page with styled header

--- portfolio-site/src/pages/BottomRackClothing.tsx
+++ portfolio-site/src/pages/BottomRackClothing.tsx
@@ -1,5 +1,20 @@
+import { Box, Heading, VStack } from "@chakra-ui/react";
+
 const BottomRackClothing = () => {
-  return <div>hello</div>;
+  return (
+    <>
+      <Box bgGradient="linear(to-l, #7928CA, green.300)">
+        <VStack justifyContent="center">
+          <Heading as="h1" size="4xl">
+            Bottom Rack Clothing
+          </Heading>
+          <Heading as="h3" size="lg">
+            Web Design & Development
+          </Heading>
+        </VStack>
+      </Box>
+    </>
+  );
 };
 
 export default BottomRackClothing;
