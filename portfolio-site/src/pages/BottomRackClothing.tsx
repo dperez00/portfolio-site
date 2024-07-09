12387184a35b699a9f2517219e3e96f0441f9671
David Perez <perez.david3001@gmail.com>
2024-07-08 22:14:41 -0500

    Add video for BottomRackClothing page

--- portfolio-site/src/pages/BottomRackClothing.tsx
+++ portfolio-site/src/pages/BottomRackClothing.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, VStack } from "@chakra-ui/react";
+import { AspectRatio, Box, Heading, VStack } from "@chakra-ui/react";
 
 const BottomRackClothing = () => {
   return (
@@ -13,6 +13,13 @@ const BottomRackClothing = () => {
           </Heading>
         </VStack>
       </Box>
+      <AspectRatio maxW="560px" ratio={16 / 9}>
+        <iframe
+          title="Bottom Rack Clothing"
+          src="https://www.loom.com/embed/10ee493c1fdb44b68b2a7ba9c12400f1?sid=5d3cef64-3486-4780-b0b2-da4cf73400cb"
+          allowFullScreen
+        />
+      </AspectRatio>
     </>
   );
 };
