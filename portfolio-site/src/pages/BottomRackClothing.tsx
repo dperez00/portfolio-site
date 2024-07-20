7635eea9292b04842a745f59a7380e8c4d5b8eb6
David Perez <perez.david3001@gmail.com>
2024-07-20 15:10:03 -0500

    BottomRackClothing page: add details to page and styling.

--- portfolio-site/src/pages/BottomRackClothing.tsx
+++ portfolio-site/src/pages/BottomRackClothing.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, VStack } from "@chakra-ui/react";
+import { Box, Heading, Text, VStack } from "@chakra-ui/react";
 
 const BottomRackClothing = () => {
   return (
@@ -17,10 +17,64 @@ const BottomRackClothing = () => {
       <Box mt="80px" display="flex" justifyContent="center" alignItems="center">
         <iframe
           src="https://www.loom.com/embed/10ee493c1fdb44b68b2a7ba9c12400f1?sid=3d14baad-6654-40a2-b842-790fbcc05e3f"
-          width="560px"
-          height="315px"
+          width="900px"
+          height="600px"
           allowFullScreen
         />
+      </Box>
+
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <VStack align="start" maxWidth="800px">
+          <Text as="h3" fontSize="24px" mt="20px" mb="10px" fontWeight="bold">
+            Who
+          </Text>
+          <Text as="p" fontSize="18px">
+            Bottom Rack Clothing is a clothing/accessories brand. It holds a
+            large collection of clothes and accessories with a wide range of
+            variants.
+          </Text>
+          <Text as="h3" fontSize="24px" mt="20px" mb="10px" fontWeight="bold">
+            The Challenge
+          </Text>
+          <Text as="p" fontSize="18px">
+            Scaling a clothing brand with a large inventory of clothes and
+            accessories can be very difficult with a brand of this size. With
+            such a large inventory, it is important to stay organized. Staying
+            organized is crucial when it comes to any business and future
+            scaling.
+            <br />
+            <br />
+            With large inventories, you want fast and easy to find search
+            options for the best customer experience. When customers can't find
+            what they are looking for, you run the risk of losing potential
+            customers and growth.
+          </Text>
+          <Text as="h3" fontSize="24px" mt="20px" mb="10px" fontWeight="bold">
+            The Solution
+          </Text>
+          <Text as="p" fontSize="18px">
+            Using a dynamic premium theme, I was able to implement a filter and
+            search feature, making it easier for customers to find what they
+            want quickly. Not only does this help with a stress-free shopping
+            experience, but it also helps with conversions.
+            <br />
+            <br />I also implemented a "Shop the look" feature. This benefits
+            the business by quickly advertising possible combined products. It
+            shows a selection of a few items that are often bought together and
+            allows for quick navigation to the product. the image depicts these
+            products as separate parts of one "look". This feature encourages
+            the customer to purchase multiple products at once.
+          </Text>
+          <Text as="h3" fontSize="24px" mt="20px" mb="10px" fontWeight="bold">
+            The Result
+          </Text>
+          <Text as="p" fontSize="18px">
+            With limitless customization possibilities, Bottom Rack Clothing has
+            been built with the ability to scale in the long-term and help
+            streamline many difficult tasks. I built this website with emphasis
+            on customer visual experience and ease of use
+          </Text>
+        </VStack>
       </Box>
     </>
   );
