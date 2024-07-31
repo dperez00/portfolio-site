84092461c41e0bd3f4440f5734289c65b8b59158
David Perez <perez.david3001@gmail.com>
2024-07-30 23:11:44 -0500

    update page details

--- portfolio-site/src/pages/CosmosForge.tsx
+++ portfolio-site/src/pages/CosmosForge.tsx
@@ -61,7 +61,7 @@ const CosmosForge = () => {
       <Box bgGradient="linear(to-l, #7928CA, green.300)">
         <VStack justifyContent="center">
           <Heading as="h1" size="4xl">
-            Bottom Rack Clothing
+            Cosmos Forge
           </Heading>
           <Heading as="h3" size="lg">
             Web Design & Development
@@ -71,7 +71,7 @@ const CosmosForge = () => {
 
       <Box mt="80px" display="flex" justifyContent="center" alignItems="center">
         <iframe
-          src="https://www.loom.com/embed/10ee493c1fdb44b68b2a7ba9c12400f1?sid=3d14baad-6654-40a2-b842-790fbcc05e3f"
+          src="https://www.loom.com/embed/35f4875efd5c4bc38446d432f6cdea99?sid=6afc37e7-ac1e-422d-803c-a1cdbed438d0"
           width="900px"
           height="600px"
           allowFullScreen
@@ -84,50 +84,40 @@ const CosmosForge = () => {
             Who
           </Text>
           <Text as="p" fontSize="18px">
-            Bottom Rack Clothing is a clothing/accessories brand. It holds a
-            large collection of clothes and accessories with a wide range of
-            variants.
+            Cosmos Forge is a store that sells galaxy projectors. It projects an
+            array of colorful stars and galaxies onto ceilings and walls.
           </Text>
           <Text as="h3" fontSize="24px" mt="20px" mb="10px" fontWeight="bold">
             The Challenge
           </Text>
           <Text as="p" fontSize="18px">
-            Scaling a clothing brand with a large inventory of clothes and
-            accessories can be very difficult with a brand of this size. With
-            such a large inventory, it is important to stay organized. Staying
-            organized is crucial when it comes to any business and future
-            scaling.
+            This website was to accommodate their sales from their Instagram.
+            They wanted an easy and adaptable page that could be linked to their
+            Instagram. No premium theme was used for this site, so it required a
+            lot of customization.
             <br />
             <br />
-            With large inventories, you want fast and easy to find search
-            options for the best customer experience. When customers can't find
-            what they are looking for, you run the risk of losing potential
-            customers and growth.
+            This case, linking Instagram required custom integration. Some other
+            features needing customization were the UX/UI layout and the FAQ
+            page.
           </Text>
           <Text as="h3" fontSize="24px" mt="20px" mb="10px" fontWeight="bold">
             The Solution
           </Text>
           <Text as="p" fontSize="18px">
-            Using a dynamic premium theme, I was able to implement a filter and
-            search feature, making it easier for customers to find what they
-            want quickly. Not only does this help with a stress-free shopping
-            experience, but it also helps with conversions.
-            <br />
-            <br />I also implemented a "Shop the look" feature. This benefits
-            the business by quickly advertising possible combined products. It
-            shows a selection of a few items that are often bought together and
-            allows for quick navigation to the product. the image depicts these
-            products as separate parts of one "look". This feature encourages
-            the customer to purchase multiple products at once.
+            The landing page showcases a minimalistic custom design and a
+            image-text layout for easily adding images with text. Instagram was
+            integrated to showcase their pictures/post from their customers on
+            the website. To prevent clutter in the FAQ section, I created an
+            accordion page using custom coding.
           </Text>
           <Text as="h3" fontSize="24px" mt="20px" mb="10px" fontWeight="bold">
             The Result
           </Text>
           <Text as="p" fontSize="18px">
-            With limitless customization possibilities, Bottom Rack Clothing has
-            been built with the ability to scale in the long-term and help
-            streamline many difficult tasks. I built this website with emphasis
-            on customer visual experience and ease of use
+            Cosmos Forge now has an easy to maintain and adaptable store to
+            their existing Instagram presence. They now have the ability to
+            easily replace images/text and showcase customer posts/reviews.
           </Text>
         </VStack>
       </Box>
