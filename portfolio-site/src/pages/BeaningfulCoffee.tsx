176d7812e5c14c97cc3084f60231059247acbd07
David Perez <perez.david3001@gmail.com>
2024-07-31 21:19:26 -0500

    Add details to Beaningful Coffee page.

--- portfolio-site/src/pages/BeaningfulCoffee.tsx
+++ portfolio-site/src/pages/BeaningfulCoffee.tsx
@@ -61,17 +61,17 @@ const BeaningfulCoffee = () => {
       <Box bgGradient="linear(to-l, #7928CA, green.300)">
         <VStack justifyContent="center">
           <Heading as="h1" size="4xl">
-            Bottom Rack Clothing
+            Beaningful Coffee
           </Heading>
           <Heading as="h3" size="lg">
-            Web Design & Development
+            Branding & Web Design
           </Heading>
         </VStack>
       </Box>
 
       <Box mt="80px" display="flex" justifyContent="center" alignItems="center">
         <iframe
-          src="https://www.loom.com/embed/10ee493c1fdb44b68b2a7ba9c12400f1?sid=3d14baad-6654-40a2-b842-790fbcc05e3f"
+          src="https://www.loom.com/embed/1441631152854ba5b437e7b5bd8f6408?sid=bd8f217a-7f11-4205-b4e3-2a3cbbe7506c"
           width="900px"
           height="600px"
           allowFullScreen
@@ -84,50 +84,44 @@ const BeaningfulCoffee = () => {
             Who
           </Text>
           <Text as="p" fontSize="18px">
-            Bottom Rack Clothing is a clothing/accessories brand. It holds a
-            large collection of clothes and accessories with a wide range of
-            variants.
+            Beaningful Coffee is an online coffee shop. It holds a variety of
+            coffee aromas and even has a subscription for customers who want to
+            enjoy the coffee of the month.
           </Text>
           <Text as="h3" fontSize="24px" mt="20px" mb="10px" fontWeight="bold">
             The Challenge
           </Text>
           <Text as="p" fontSize="18px">
-            Scaling a clothing brand with a large inventory of clothes and
-            accessories can be very difficult with a brand of this size. With
-            such a large inventory, it is important to stay organized. Staying
-            organized is crucial when it comes to any business and future
-            scaling.
-            <br />
-            <br />
-            With large inventories, you want fast and easy to find search
-            options for the best customer experience. When customers can't find
-            what they are looking for, you run the risk of losing potential
-            customers and growth.
+            Being in the specialty eatery industry is very competitive.
+            Especially when you have companies like Starbucks, Dunkin' Donuts,
+            and 7 Brew to compete with. Not to mention other eCommerce coffee
+            brands. A store like this will need a site that stands out from
+            other coffee sites and provides great features that customers will
+            enjoy.
           </Text>
           <Text as="h3" fontSize="24px" mt="20px" mb="10px" fontWeight="bold">
             The Solution
           </Text>
           <Text as="p" fontSize="18px">
-            Using a dynamic premium theme, I was able to implement a filter and
-            search feature, making it easier for customers to find what they
-            want quickly. Not only does this help with a stress-free shopping
-            experience, but it also helps with conversions.
-            <br />
-            <br />I also implemented a "Shop the look" feature. This benefits
-            the business by quickly advertising possible combined products. It
-            shows a selection of a few items that are often bought together and
-            allows for quick navigation to the product. the image depicts these
-            products as separate parts of one "look". This feature encourages
-            the customer to purchase multiple products at once.
+            To make the site stand out more, I used a mixture of the Shopify
+            Dawn theme, custom code, and premium pictures. The premium pictures
+            will help the page sections “pop” more and set the tone for the
+            site. Using custom code, I implemented a custom metafield section
+            that is dynamic and includes a meter to track “acidity” and
+            “strength” of the coffee products. I also implemented a custom
+            template to the “about us” section that alternates the image and
+            text with each new section to make reading easier and more appealing
+            to the eye.
           </Text>
           <Text as="h3" fontSize="24px" mt="20px" mb="10px" fontWeight="bold">
             The Result
           </Text>
           <Text as="p" fontSize="18px">
-            With limitless customization possibilities, Bottom Rack Clothing has
-            been built with the ability to scale in the long-term and help
-            streamline many difficult tasks. I built this website with emphasis
-            on customer visual experience and ease of use
+            Beaningful Coffee is now completely dynamic. With an emphasis on
+            ease of use, the custom code I implemented now makes it easier for
+            the store owner to customize every page and section, empowering them
+            to make easy changes on their own time. It also has a great mix of
+            images, color, and features that make the entire store stand out.
           </Text>
         </VStack>
       </Box>
