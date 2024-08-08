8a6f173e9e66c3e7dd1964c92ff9edd6336bb6a1
David Perez <perez.david3001@gmail.com>
2024-08-07 21:39:05 -0500

    Add Magtech and GameHub pages to routes.tsx

--- portfolio-site/src/routes.tsx
+++ portfolio-site/src/routes.tsx
@@ -5,6 +5,8 @@ import BottomRackClothing from "./pages/BottomRackClothing";
 import CosmosForge from "./pages/CosmosForge";
 import CosmosForgeAffiliate from "./pages/CosmosForgeAffiliate";
 import BeaningfulCoffee from "./pages/BeaningfulCoffee";
+import Magtech from "./pages/Magtech";
+import GameHub from "./pages/GameHub";
 
 /* We switched from using "Router" in this component to using "Switch" and "Route".
 We did this because we can't use "Router" in this component and in our Main.tsx file.
@@ -22,8 +24,8 @@ const AppRouter = () => {
           element={<CosmosForgeAffiliate />}
         />
         <Route path="/BeaningfulCoffee" element={<BeaningfulCoffee />} />
-        <Route path="/Magtech" element={<BeaningfulCoffee />} />
-        <Route path="/GameHub" element={<BeaningfulCoffee />} />
+        <Route path="/Magtech" element={<Magtech />} />
+        <Route path="/GameHub" element={<GameHub />} />
         {/* add more routes for other projects if neccessary */}
       </Routes>
     </>
