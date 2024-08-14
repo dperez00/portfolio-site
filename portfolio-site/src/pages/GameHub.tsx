50ecb6187cbaeeb0ebe2b77952128264df29e2c1
David Perez <perez.david3001@gmail.com>
2024-08-13 23:18:42 -0500

    Update GameHub page with project details and technologies used.

--- portfolio-site/src/pages/GameHub.tsx
+++ portfolio-site/src/pages/GameHub.tsx
@@ -87,36 +87,47 @@ const GameHub = () => {
             Who
           </Text>
           <Text as="p" fontSize="18px">
-            <Link href="https://magtech.dev/" isExternal color="#67d391">
-              Magteach Development
+            Of all my projects, my{" "}
+            <Link
+              href="https://game-hub-1-five.vercel.app/"
+              isExternal
+              color="#67d391"
+            >
+              GameHub
             </Link>{" "}
-            is my first portfolio I created using Webflow and custom code.
+            stands out as my favorite. This project was developed to demonstrate
+            my proficiency in React and TypeScript, along with other
+            technologies.
           </Text>
           <Text as="h3" fontSize="24px" mt="20px" mb="10px" fontWeight="bold">
             The Challenge
           </Text>
           <Text as="p" fontSize="18px">
-            The challenge was to create a portfolio using the skills I have
-            learned while also learning how to use a site like Webflow. Another
-            challenge was to make the site look professional and easy to
-            navigate.
+            The challenge was to utilize new technologies to develop a GameHub
+            capable of searching an extensive database of games, displaying
+            detailed game pages with images and trailers (when available), and
+            displaying the platforms each game is available on.
           </Text>
           <Text as="h3" fontSize="24px" mt="20px" mb="10px" fontWeight="bold">
             The Solution
           </Text>
           <Text as="p" fontSize="18px">
-            The solution was to create a site that was easy to navigate and
-            professional. I used custom code to make the site more dynamic and
-            added animations to make the site more engaging.
+            The solution involved leveraging technologies such as React,
+            TypeScript, React Query, React Router, Chakra UI, and Zustand to
+            develop a visually appealing and responsive website.
           </Text>
           <Text as="h3" fontSize="24px" mt="20px" mb="10px" fontWeight="bold">
             The Result
           </Text>
           <Text as="p" fontSize="18px">
-            The result was a professional and engaging portfolio that showcases
-            my skills and projects. The site is easy to navigate and looks great
-            on all devices. For a more in-depth look at my first portfolio,
-            click the "Magtech Development" link above.
+            The outcome was a visually appealing and responsive website that
+            effectively demonstrated my proficiency in React and TypeScript.
+            Utilizing the RAWG API, I retrieved and displayed game data on the
+            site. Chakra UI was employed to ensure the site was both
+            aesthetically pleasing and responsive. React Query was integrated to
+            cache data, enhancing the site's performance. Zustand was used for
+            state management, and React Router facilitated seamless navigation
+            throughout the site.
           </Text>
         </VStack>
       </Box>
