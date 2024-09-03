488f578fb0001112d5944c626116027a1012c6b7
David Perez <perez.david3001@gmail.com>
2024-09-02 21:09:56 -0500

    Refactor Skills component for responsiveness.

--- portfolio-site/src/components/Skills.tsx
+++ portfolio-site/src/components/Skills.tsx
@@ -9,6 +9,7 @@ import {
   Heading,
   SimpleGrid,
   Text,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { useState } from "react";
 
@@ -42,30 +43,38 @@ const Skills = () => {
     setPage((prevPage) => (prevPage + 1) % skills.length);
   };
 
+  const columns = useBreakpointValue({ base: 1, sm: 2, md: 3 });
+  const headingFontSize = useBreakpointValue({ base: "32px", md: "48px" });
+  const subHeadingFontSize = useBreakpointValue({ base: "24px", md: "28px" });
+  const textFontSize = useBreakpointValue({ base: "16px", md: "18px" });
+  const cardMaxWidth = useBreakpointValue({ base: "100%", md: "850px" });
+  const cardMarginBottom = useBreakpointValue({ base: "100px", md: "250px" });
+  const gridSpacing = useBreakpointValue({ base: 5, md: 14 });
+
   return (
     <>
       <HStack justifyContent="center">
-        <Text fontSize="18px">Explore My</Text>
+        <Text fontSize={textFontSize}>Explore My</Text>
       </HStack>
       <HStack justifyContent="center">
-        <Heading as="h1" fontSize="48px" color="#67d391" mb="32px">
+        <Heading as="h1" fontSize={headingFontSize} color="#67d391" mb="32px">
           Experience
         </Heading>
       </HStack>
       <Card
         borderRadius="6px"
-        maxW="850px"
+        maxW={cardMaxWidth}
         mx="auto"
-        mb="250px"
+        mb={cardMarginBottom}
         alignItems="center"
       >
         <CardHeader>
-          <Heading as="h2" fontSize="28px" mb="32px">
+          <Heading as="h2" fontSize={subHeadingFontSize} mb="32px">
             Frontend Development & Tools
           </Heading>
         </CardHeader>
         <CardBody>
-          <SimpleGrid columns={3} spacing={14}>
+          <SimpleGrid columns={columns} spacing={gridSpacing}>
             {skills[page].map((skill) => (
               <HStack key={skill.name}>
                 <CheckCircleIcon color="#67d391" />
@@ -78,7 +87,7 @@ const Skills = () => {
         </CardBody>
         <CardFooter>
           <Button
-            bgGradient="linear(to-1, #7928CA, green.300)"
+            bgGradient="linear(to-l, #7928CA, green.300)"
             onClick={handleNext}
           >
             Next
