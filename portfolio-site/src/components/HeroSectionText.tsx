502f9cebff86d5c161737654e0dd2954202ac674
David Perez <perez.david3001@gmail.com>
2024-09-10 11:39:36 -0500

    Make icons link back to respective accounts and make text responsive.

--- portfolio-site/src/components/HeroSectionText.tsx
+++ portfolio-site/src/components/HeroSectionText.tsx
@@ -2,6 +2,7 @@ import { Box, HStack, Heading, Text, VStack } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const HeroSectionText = () => {
   const [word, setWord] = useState("React Developer");
@@ -24,10 +25,10 @@ const HeroSectionText = () => {
   return (
     <>
       <VStack>
-        <Heading as="h1" size="4xl">
+        <Heading as="h1" fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}>
           David Perez
         </Heading>
-        <Text fontSize="4xl">
+        <Text fontSize={{ base: "2xl", md: "3xl", lg: "4xl" }}>
           I'm a{" "}
           <Box
             as="span"
@@ -38,20 +39,24 @@ const HeroSectionText = () => {
           </Box>
         </Text>
         <HStack mt={4} spacing={4}>
-          <Box
-            borderRadius="full"
-            bgGradient="linear(to-l, #7928CA, green.300)"
-            p={1}
-          >
-            <FaLinkedin size="20px" />
-          </Box>
-          <Box
-            borderRadius="full"
-            bgGradient="linear(to-l, #7928CA, green.300)"
-            p={1}
-          >
-            <FaGithub size="20px" />
-          </Box>
+          <Link to="https://www.linkedin.com/in/davidperez00/" target="_blank">
+            <Box
+              borderRadius="full"
+              bgGradient="linear(to-l, #7928CA, green.300)"
+              p={1}
+            >
+              <FaLinkedin size="20px" />
+            </Box>
+          </Link>
+          <Link to="https://github.com/dperez00" target="_blank">
+            <Box
+              borderRadius="full"
+              bgGradient="linear(to-l, #7928CA, green.300)"
+              p={1}
+            >
+              <FaGithub size="20px" />
+            </Box>
+          </Link>
         </HStack>
       </VStack>
     </>
